3034c91f6cf097b57531091f30cb585504ddfc81
kky04994 <kkyo8747@naver.com>
2022-02-14 03:55:31 +0900

    ⚙️ (#12) Theme 파일 추가

---
+++ today/src/style/Theme.ts
@@ -0,0 +1,33 @@
+export const colors = {
+  pink: "FFA1A1",
+  green: "77CEB4",
+  red: "D60000",
+  grayscale: {
+    lightGray1: "F5F5F5",
+    lightGray2: "EBEDF0",
+    gray: "C4C4C4",
+    dark: "848484",
+    white: "ffffff",
+    black: "000000",
+  },
+};
+
+export const fonts = {
+  point: "80px",
+  h1: "60px",
+  h2: "40px",
+  h3: "30px",
+  subTitle: "28px",
+  body1: "25px",
+  body2: "22px",
+  body3: "20px",
+  body4: "18px",
+  small: "15px",
+};
+
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
