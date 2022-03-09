05e5a29aefbba391a8ac4d86d459a90faa32ad40
kky04994 <kkyo8747@naver.com>
2022-03-09 22:46:56 +0900

    📑 (#17) - Header style 파일 추가

---
+++ today/src/components/Header/style.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 73px;
+  background: #000000;
+  padding-top: 18px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 90%;
+  height: 40px;
+  justify-content: space-between;
+`;
+
+export const LeftContent = styled.div`
+  width: 334px;
+  height: 40px;
+  display: flex;
+`;
+
+export const RightContent = styled.div`
+  width: 580px;
+  height: 40px;
+  justify-content: space-between;
+  display: flex;
+`;
+
+export const SearchBar = styled.div`
+  width: 227px;
+  height: 34px;
+  border: 1px solid #ffffff;
+  border-radius: 15px;
+  padding-right: 8px;
+  margin-left: 45px;
+  display: flex;
+  justify-content: space-between;
+  img {
+    width: 15px;
+    height: 15px;
+    margin-top: 8px;
+  }
+  input {
+    width: 186px;
+    background: #000000;
+    border-radius: 15px;
+    outline: none;
+    border: none;
+    color: #ffffff;
+    margin-left: 5px;
+  }
+`;
+
+export const Text = styled.div`
+  width: 114px;
+  height: 20px;
+  margin-top: 9px;
+  display: flex;
+  font-size: 20px;
+  color: #ffffff;
+`;
+
+export const MailBox = styled.div`
+  width: 265px;
+  padding-top: 3px;
+  height: 34px;
+  border: 1px solid #ffffff;
+  border-radius: 8px;
+  color: #ffffff;
+  text-align: center;
+`;
