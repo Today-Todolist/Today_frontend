da8a7f6db04fbb14865516bdcdcbfb0780f9b170
kky04994 <kkyo8747@naver.com>
2022-03-20 16:04:53 +0900

    📑 (#21) - SearchContainer 파일 추가

---
+++ today/src/container/Search/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import Search from "../../components/Search";
+
+const SearchContainer = () => {
+  return (
+    <>
+      <Header />
+      <Search />
+    </>
+  );
+};
+
+export default SearchContainer;
