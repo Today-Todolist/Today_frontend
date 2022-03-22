ec6c9bb62e763a701d6fc47105f0673ebf2e0b9f
kky04994 <kkyo8747@naver.com>
2022-03-22 10:14:24 +0900

    ✅ (#24) - 실행 시 오류나는 부분 고침

--- today/src/container/ModifyProfile/index.tsx
+++ today/src/container/ModifyProfile/index.tsx
@@ -1,4 +1,4 @@
-import Header from "../../components/Header/Header";
+import Header from "../../components/Header";
 import ModifyProfile from "../../components/ModifyProfile";
 
 const ModifyProfileContainer = () => {
