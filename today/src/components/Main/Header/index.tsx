47d9aec841c34d70f76b4ccc15004c0b51163873
kky04994 <kkyo8747@naver.com>
2022-04-04 09:46:40 +0900

    📑 (#7) - signupmodal 코드 추가

--- today/src/components/Main/Header/index.tsx
+++ today/src/components/Main/Header/index.tsx
@@ -1,21 +1,49 @@
 import * as S from "./style";
-import { HeaderLogo } from "../../../assets/Logo";
+import { HeaderLogo, SignupLogo } from "../../../assets/Logo";
+import { useState } from "react";
+import Modal from "react-modal";
 
 const Header = () => {
+  const [isOpenSignupModal, setOpenSignupModal] = useState<boolean>(false);
+
+  const onClickModal = () => {
+    setOpenSignupModal(!isOpenSignupModal);
+  };
+
   return (
-    <S.HeaderContainer>
-      <S.Content>
-        <S.LeftContent>
-          <img src={HeaderLogo} />
-          <span>오늘이란?</span>
-          <span>오늘 팀</span>
-        </S.LeftContent>
-        <S.RightContent>
-          <span>회원가입</span>
-          <S.LoginBtn>로그인</S.LoginBtn>
-        </S.RightContent>
-      </S.Content>
-    </S.HeaderContainer>
+    <>
+      <S.HeaderContainer>
+        <S.Content>
+          <S.LeftContent>
+            <img src={HeaderLogo} />
+            <span>오늘이란?</span>
+            <span>오늘 팀</span>
+          </S.LeftContent>
+          <S.RightContent>
+            <span onClick={onClickModal}>회원가입</span>
+            <S.LoginBtn>로그인</S.LoginBtn>
+          </S.RightContent>
+        </S.Content>
+      </S.HeaderContainer>
+      <S.ModalContainer>
+        <Modal
+          style={S.Modal}
+          overlayClassName={S.ModalContainer.Overlay}
+          isOpen={isOpenSignupModal}
+          onRequestClose={onClickModal}
+        >
+          <S.SignupContainer>
+            <S.Logo src={SignupLogo} />
+            <S.ClcikContent>
+              <S.Inputs placeholder="이메일" />
+              <S.Inputs placeholder="비밀번호" />
+              <S.Inputs placeholder="닉네임" />
+              <S.SubBtn>다음</S.SubBtn>
+            </S.ClcikContent>
+          </S.SignupContainer>
+        </Modal>
+      </S.ModalContainer>
+    </>
   );
 };
 
