72be827700f8587d042502f194c2a0ea7a019a08
kky04994 <kkyo8747@naver.com>
2022-04-04 09:44:50 +0900

    📑 (#7) - Signup 파일 추가

---
+++ today/src/components/Signup/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./style";
+
+const Signup = () => {
+  return (
+    <S.SignupContainer>
+      <S.Content></S.Content>
+    </S.SignupContainer>
+  );
+};
+
+export default Signup;
