c81deb423bd013a2ae3b64fd6581429317b1cf1e
kky04994 <kkyo8747@naver.com>
2022-05-17 11:34:55 +0900

    📑 (#29) - email compontents 파일 추가

---
+++ today/src/components/Email/style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const EmailBackground = styled.div`
+  background: #000000;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  width: 364px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid red;
+  text-align: center;
+`;
+
+export const TextBox = styled.div`
+  border: 1px solid blue;
+  margin: 40px auto 0px;
+`;
+
+export const Font = styled.div`
+  font-family: "SeoulHangangCBL";
+  color: #ffffff;
+  font-size: 20px;
+  margin-top: 9px;
+`;
