f246dcf74bd9b7789c2134013662acea5b681074
kky04994 <kkyo8747@naver.com>
2022-05-19 20:26:15 +0900

    📑 (#40) - 친구 목록, 친구 신청 목록, 추천 템플릿, 나의 템플릿 api 추가

--- today/src/util/api/User.ts
+++ today/src/util/api/User.ts
@@ -31,6 +31,43 @@ export interface ListFriends {
   contents: FriendContent[];
 }
 
-export const getListFriends = async (email: string) => {
-  return await request.get<ListFriends>(baseURL + `${email}` + `/friends/`);
+export const getListFriends = async (
+  email: string,
+  page: number,
+  size: number
+) => {
+  return await request.get<ListFriends>(
+    baseURL + `${email}` + `/friends/?page=` + `${page}` + `&size=` + `${size}`
+  );
+};
+
+export const getListFriendsApply = async (page: number, size: number) => {
+  return await request.get<ListFriends>(
+    baseURL + `friend-applys?page=` + `${page}` + `&size=` + `${size}`
+  );
+};
+
+export interface Template {
+  id: string;
+  title: string;
+  profile: string;
+}
+
+export interface User {
+  email: string;
+  nickname: string;
+  profile: string;
+}
+
+export interface Templates {
+  user: User;
+  template: Template;
+}
+
+export const getRecommTemplates = async () => {
+  return await request.get<Templates>(baseURL + `random-template?size=3`);
+};
+
+export const getMyTemplates = async () => {
+  return await request.get<Template>(baseURL + `templates`);
 };
