2358a90d5ded3df4f31e29167eca8e3b0765ec57
kky04994 <kkyo8747@naver.com>
2022-06-29 23:24:07 +0900

    📑 (#40) - 내 친구 목록 연동 처리

--- today/src/components/Mypage/UserContent/Friend/index.tsx
+++ today/src/components/Mypage/UserContent/Friend/index.tsx
@@ -1,22 +1,29 @@
+import { useListFriends, useMyProfile } from "../../../../queries/User";
 import * as S from "./style";
 
 const Friend = () => {
+  const { data: emailData } = useMyProfile();
+  const { data } = useListFriends(emailData?.data.email || "", 1, 100);
+
   return (
     <S.ContentList>
-      {new Array(8).fill(0).map((_, index) => (
-        <S.ContentBox>
-          <S.Content>
-            <div>
-              <S.Profile></S.Profile>
-              <S.NickName>제주도가이드</S.NickName>
-              <S.Email>jejuisland@gmail.com</S.Email>
-            </div>
-            <div>
-              <S.DeleteBtn>친구 삭제</S.DeleteBtn>
-            </div>
-          </S.Content>
-        </S.ContentBox>
-      ))}
+      {data?.data.contents.map((item, index) => {
+        return (
+          <S.ContentBox>
+            <S.Content key={index}>
+              <div>
+                <S.Profile />
+                <S.NickName>{item.nickname}</S.NickName>
+                <S.Email>{item.email}</S.Email>
+              </div>
+              <div>
+                <S.DeleteBtn>친구 삭제</S.DeleteBtn>
+              </div>
+            </S.Content>
+          </S.ContentBox>
+        );
+      })}
+      {data?.data.contents === [] ? <div>현재 친구가 없습니다.</div> : null}
     </S.ContentList>
   );
 };
