d9b34780a82e51c8623de5b267fd13330aa5fbc3
kky04994 <kkyo8747@naver.com>
2022-07-06 23:40:56 +0900

    📑 (#47) - Search 연동에 맞게 각각 data 처리

--- today/src/components/Search/Email/index.tsx
+++ today/src/components/Search/Email/index.tsx
@@ -1,27 +1,36 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useSearchEmail } from "../../../queries/Search";
 import * as S from "./style";
 
 const Email = () => {
+  const [searchParams] = useSearchParams();
+  const searchWord = useMemo(() => searchParams.get("q") || "", [searchParams]);
+  const { data } = useSearchEmail(searchWord);
+
   return (
     <>
       <S.TitleBox>
-        <S.Title>2건의 결과</S.Title>
+        <S.Title>{data?.data.length}건의 결과</S.Title>
         <S.SubTitle>유저의 이메일로 검색할 결과입니다.</S.SubTitle>
       </S.TitleBox>
       <S.ContentList>
-        {new Array(8).fill(0).map((_, index) => (
-          <S.ContentBox>
-            <S.Content>
-              <div>
-                <S.Profile></S.Profile>
-                <S.NickName>제주도가이드</S.NickName>
-                <S.Email>jejuisland@gmail.com</S.Email>
-              </div>
-              <div>
-                <S.ApplyBtn>친구 신청</S.ApplyBtn>
-              </div>
-            </S.Content>
-          </S.ContentBox>
-        ))}
+        {data?.data.map((item, index) => {
+          return (
+            <S.ContentBox>
+              <S.Content key={index}>
+                <div>
+                  <S.Profile src={item.profile} />
+                  <S.NickName>{item.nickname}</S.NickName>
+                  <S.Email>{item.email}</S.Email>
+                </div>
+                <div>
+                  <S.ApplyBtn>친구 신청</S.ApplyBtn>
+                </div>
+              </S.Content>
+            </S.ContentBox>
+          );
+        })}
       </S.ContentList>
     </>
   );
--- today/src/components/Search/NickName/index.tsx
+++ today/src/components/Search/NickName/index.tsx
@@ -1,27 +1,36 @@
+import { useEffect, useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useSearchNickName } from "../../../queries/Search";
 import * as S from "./style";
 
 const NickName = () => {
+  const [searchParams] = useSearchParams();
+  const searchWord = useMemo(() => searchParams.get("q") || "", [searchParams]);
+  const { data } = useSearchNickName(searchWord);
+
   return (
     <>
       <S.TitleBox>
-        <S.Title>2건의 결과</S.Title>
+        <S.Title>{data?.data.length}건의 결과</S.Title>
         <S.SubTitle>유저의 닉네임으로 검색할 결과입니다.</S.SubTitle>
       </S.TitleBox>
       <S.ContentList>
-        {new Array(8).fill(0).map((_, index) => (
-          <S.ContentBox>
-            <S.Content>
-              <div>
-                <S.Profile></S.Profile>
-                <S.NickName>제주도가이드</S.NickName>
-                <S.Email>jejuisland@gmail.com</S.Email>
-              </div>
-              <div>
-                <S.ApplyBtn>친구 신청</S.ApplyBtn>
-              </div>
-            </S.Content>
-          </S.ContentBox>
-        ))}
+        {data?.data.map((item, index) => {
+          return (
+            <S.ContentBox>
+              <S.Content key={index}>
+                <div>
+                  <S.Profile src={item.profile} />
+                  <S.NickName>{item.nickname}</S.NickName>
+                  <S.Email>{item.email}</S.Email>
+                </div>
+                <div>
+                  <S.ApplyBtn>친구 신청</S.ApplyBtn>
+                </div>
+              </S.Content>
+            </S.ContentBox>
+          );
+        })}
       </S.ContentList>
     </>
   );
