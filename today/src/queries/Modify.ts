e3b33fa4e32c986e29e2d576cf8d1498a78865db
kky04994 <kkyo8747@naver.com>
2022-07-25 20:17:03 +0900

    📑 (#48) - 계정 삭제, 투두초기화 query 코드 추가

--- today/src/queries/Modify.ts
+++ today/src/queries/Modify.ts
@@ -2,6 +2,8 @@ import { useMutation } from "react-query";
 import {
   changeNickName,
   changePassword,
+  deleteAcccount,
+  initializationTodo,
   PasswordType,
   uploadImg,
 } from "../util/api/Modify";
@@ -16,4 +18,12 @@ export const useChangeNickName = () => {
 
 export const useChangePassword = () => {
   useMutation((data: PasswordType) => changePassword(data), {});
+};
+
+export const useIitializationTodo = () => {
+  useMutation((password: string) => initializationTodo(password));
+};
+
+export const useDeleteAcccount = () => {
+  useMutation((password: string) => deleteAcccount(password));
 };
